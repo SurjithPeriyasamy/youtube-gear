47f15ecd019a47664ec0673760356ea07f45af57
Surjith <mahindhiran005@gmail.com>
2024-02-13 14:39:31 +0530

    fixing the filterbuttons slide

--- src/components/Filterbuttons.js
+++ src/components/Filterbuttons.js
@@ -17,17 +17,17 @@ const Filterbuttons = () => {
     setCategories(json.items);
   };
   const handleNext = () => {
-    setCount(count < 8 ? count + 1 : 8);
+    setCount((prev) => (prev < 7 ? prev + 1 : 7));
   };
   const handlePrev = () => {
-    setCount(count > 0 ? count - 1 : 0);
+    setCount(count > 1 ? count - 1 : 0);
   };
   return (
     <div className="md:max-w-screen-lg w-screen mx-auto relative">
       <div className="relative z-10 lg:w-[90%] w-4/5 mx-auto overflow-hidden">
         <div
           className="flex duration-500 w-[200px]"
-          style={{ transform: `translateX(-${count * 38}%)` }}
+          style={{ transform: `translateX(-${count * 200}%)` }}
         >
           {categories.map((button, index) => (
             <Button name={button.snippet.title} key={index} />
@@ -45,7 +45,7 @@ const Filterbuttons = () => {
         </span>
         <span
           className={`${
-            count === 8 && "opacity-50 cursor-auto"
+            count === 7 && "opacity-50 cursor-auto"
           } bg-black -translate-x-6 cursor-pointer text-white shadow-lg bg-opacity-60 px-1 py-2 rounded-lg`}
           onClick={handleNext}
         >
